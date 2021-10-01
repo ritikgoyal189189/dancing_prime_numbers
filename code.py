7f25cabe0d867ed2635dfa786dadfea8ed42f7b1
Ritik <52455027+ritikgoyal189@users.noreply.github.com>
2021-10-01 22:26:24 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
 print("Your Code goes here....")
+   
+l = int(input("Enter lower range: "))  
+u = int(input("Enter upper range: "))  
+  
+for num in range(l,u + 1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num) 
